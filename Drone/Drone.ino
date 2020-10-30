eae6a8cc75c3f795ada297c99b0feefc067b2905
Blakesands <71108094+Blakesands@users.noreply.github.com>
2020-10-30 00:37:22 +0000

    Add files via upload

--- Drone/Drone.ino
+++ Drone/Drone.ino
@@ -9,53 +9,61 @@
 
 // global variables
 
-int count = 0;
+// debugging serial output counter
+//int count = 0;
 
-// arrays for setting freq range, 
-int sgnl[2] = {false, true}; // Signal on/off
-int wave[2] = {SINE_WAVE, TRIANGLE_WAVE}; // Fundamental frquency waveform not using the square waves  SQUARE_WAVE, HALF_SQUARE_WAVE
-int hwave[2] = {SINE_WAVE, TRIANGLE_WAVE}; // harmonic waveform not using the square waves  SQUARE_WAVE, HALF_SQUARE_WAVE
+// arrays for setting freq range, multiplier, trim, signal on/off, waveform and phase values. 
+int sgnl[2] = {false, true};                  // Signal on/off
+int wave[2] = {SINE_WAVE, TRIANGLE_WAVE};     // Fundamental frequency waveform not using the square waves  SQUARE_WAVE, HALF_SQUARE_WAVE
+int hwave[2] = {SINE_WAVE, TRIANGLE_WAVE};    // Harmonic waveform not using the square waves  SQUARE_WAVE, HALF_SQUARE_WAVE
 int freq[21] = {233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740}; // Fundamental frequency
-int hmon[7] = {2, 3, 4, 5, 6, 7, 8}; // harmonic content hz multiplier on harmonics
+int hmon[7] = {2, 3, 4, 5, 6, 7, 8};          // Harmonic channel hz multiplier
 int trim[67] = {-212, -106, -86, -66, -62, -58, -56, -54, -52, -48, -44, -40, -38, -36, -34, -32, -30, -28, 
                 -26, -24, -22, -20, -18, -16, -14, -12, -10, -8, -6, -4, -3, -2, -1, 0, 1, 2, 3, 4, 6, 8, 
                 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 44, 48, 52, 56, 58, 62, 64, 66, 86, 106, 212}; //harmonic content Hz trim
 int phas[23] = {0, 0.003, 0.006, 0.011, 0.022, 0.045, 0.09, 0.18, 0.36, .72, 1.44, 2.88, 5.72, 11.5, 22.5, 45, 90, 135, 180, 225, 270, 315, 0}; //Phase on harmonics
 
-// Variables for inputs and array indexes
+// Variables for button inputs
 const int buttonPin1 = 3;     // the number of the pushbutton pin
 int buttonState1 = 0;         // current state of the button
 int lastButtonState1 = 0;     // previous state of the button
-int waveindex = 0;            // 0-1 Sine or Triangle Wave
-int lastwaveindex = 0;
 
 const int buttonPin2 = 2;     // the number of the pushbutton pin
 int buttonState2 = 0;         // current state of the button
 int lastButtonState2 = 0;     // previous state of the button
-int signalindex = 0;          // 0-1 off/on
-int lastsignalindex = 0;
 
 const int buttonPin3 = 6;     // the number of the pushbutton pin
 int buttonState3 = 0;         // current state of the button
 int lastButtonState3 = 0;     // previous state of the button
-int harmonicindex = 0;        // Harmonic multiplier for gen2
-int lastharmonicindex = 0;
 
 const int buttonPin4 = 8;     // the number of the pushbutton pin
 int buttonState4 = 0;         // current state of the button
 int lastButtonState4 = 0;     // previous state of the button
+
+// Variables for pot inputs
+const int pot2 = A5;          // input pin for the potentiometer
+const int pot = A0;           // input pin for the potentiometer
+const int pot3 = A3;          // input pin for the potentiometer
+
+// Variables for array indexes
+int waveindex = 0;            // 0-1 Sine or Triangle Wave
+int lastwaveindex = 0;
+
+int harmonicindex = 0;        // Harmonic multiplier for gen2
+int lastharmonicindex = 0;
+
+int signalindex = 0;          // 0-1 off/on
+int lastsignalindex = 0;
+
 int hwaveindex = 0;           // 0-1 Sine or Triangle Wave
 int lasthwaveindex = 0;
 
-const int pot3 = A3;          // the number of the pushbutton pin
 int phaseindex = 0;           // phase for gen2
 int lastphaseindex = 0;
 
-const int pot = A0;           // select the input pin for the potentiometer
 int freqindex = 0;            // Frequency index
 int lastfreqindex = 0;
 
-const int pot2 = A5;          // select the input pin for the potentiometer
 int trimindex = 0;            // fine trim for harmonics
 int lasttrimindex = 0;
 
@@ -66,15 +74,20 @@ AD9833 gen2(FNC_PIN2);       // Defaults to 25MHz internal reference frequency
 
 void setup() {
   
-  // This MUST be the first command after declaring the AD9833 object
+// This MUST be the first command after declaring the AD9833 object
   gen.Begin();
-  gen.Begin();
+  gen2.Begin();
+  
+// debugging waveform generator reset and disable
 //  gen.Reset();  
 //  gen2.Reset(); 
-     
-  Serial.begin(115200);
+//  gen.DisableDAC (true);
+//  gen.DisableInternalClock (true);
 
-  // button code, uncomment if not using that pin to avoid floating inputs
+// debugging serial output     
+//  Serial.begin(115200);
+
+// define input pins, comment out if not using that pin to avoid floating inputs
   pinMode(buttonPin1, INPUT);
 //  pinMode(buttonPin2, INPUT);
 //  pinMode(buttonPin3, INPUT);
@@ -82,15 +95,6 @@ void setup() {
   pinMode(pot, INPUT);
 //  pinMode(pot2, INPUT);
   pinMode(pot3, INPUT);
-    
-  // There are two register sets,
-  // REG0 and REG1.
-  // Each one can be programmed for:
-  //   Signal type - SINE_WAVE, TRIANGLE_WAVE, SQUARE_WAVE, and HALF_SQUARE_WAVE
-  //   Frequency - 0 to 12.5 MHz
-  //   Phase - 0 to 360 degress (this is only useful if it is 'relative' to some other signal
-  //           such as the phase difference between REG0 and REG1).
-  // In ApplySignal, if Phase is not given, it defaults to 0.
 
 // setup waveform generators
   gen.ApplySignal(TRIANGLE_WAVE, REG0, 440, 0); 
@@ -109,17 +113,17 @@ void loop() {
     
   freqindex = analogRead(pot); // read the potentiometer analog input pin:
   freqindex = map(freqindex, 0, 1000, 0, 20); // map pot input to array size - doesn't go up to 1023 because of jitter from the pot
-  freqindex = constrain(freqindex, 0, 20); // belt and braces
+  freqindex = constrain(freqindex, 0, 20);    // constrain index number to avoid freak inputs
   
   trimindex = analogRead(pot2); // read the potentiometer analog input pin:  
   trimindex = map(trimindex, 0, 1000, 0, 66); // map pot input to array size
-  trimindex = constrain(trimindex, 0, 66);  
+  trimindex = constrain(trimindex, 0, 66);    // constrain index number to avoid freak inputs  
  
   phaseindex = analogRead(pot3); // read the potentiometer analog input pin:  
   phaseindex = map(phaseindex, 0, 1000, 0, 22); // map pot input to array size
-  phaseindex = constrain(phaseindex, 0, 22);  
+  phaseindex = constrain(phaseindex, 0, 22);    // constrain index number to avoid freak inputs
    
-  
+// use if statements to see if a button has been pressed or pot value changed and increment the index associated, reset index to 0 if it goes over the array size 
   if (buttonState1 != lastButtonState1) { // compare the buttonState to its previous state
 
     if (buttonState1 == HIGH) {
@@ -185,8 +189,6 @@ void loop() {
     gen.ApplySignal(WS, REG0, FS-TM); // remove the -TM in this line (added for testing purposes)
     gen2.ApplySignal(HW, REG0, HM*FS-TM,PS);    
   }      
-
-
   
 // debugging serial output
 //  if (count > 10) { // 10 x 20ms delay = update serial 5/s
@@ -200,11 +202,13 @@ void loop() {
 //  count = 0;
 //  } 
 
-// save the current state as the last state, for next time through the loop 
+// save the current button state as the last state, for next time through the loop 
   lastButtonState1 = buttonState1; 
   lastButtonState2 = buttonState2;
   lastButtonState3 = buttonState3;  
   lastButtonState4 = buttonState4; 
+
+// save the current index state as the last state, for next time through the loop  
   lastfreqindex = freqindex;
   lastwaveindex = waveindex;
   lastharmonicindex = harmonicindex; 
@@ -212,7 +216,10 @@ void loop() {
   lasttrimindex = trimindex;
   lastphaseindex = phaseindex;
 
-// delay a bit and increment counter
+// delay a bit and debugging increment counter
   delay(20);
-  ++count; 
-} // loop
+//  ++count; 
+
+} // end of loop
+
+// Cheers!
